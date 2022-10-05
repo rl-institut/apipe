ecc8e5cf28ad323f8ad26413c170ac6d941f9d2a
nesnoj <jonathan.amme@rl-institut.de>
2022-10-05 13:05:12 +0200

    Add function to get dataset dir

--- digipipe/store/utils.py
+++ digipipe/store/utils.py
@@ -4,8 +4,40 @@ Helper functions for datasets
 
 import inspect
 import os
+from pathlib import Path
 from digipipe import store
 
 
-def get_abs_store_path():
-    return os.path.dirname(inspect.getfile(store))
+def get_abs_store_root_path():
+    """Get absolute path to data store
+
+    Returns
+    -------
+    PosixPath
+        Path to data store
+    """
+    return Path(os.path.dirname(inspect.getfile(store)))
+
+
+def get_abs_dataset_path(category, name, data_dir=True):
+    """Get absolute path to a dataset
+
+    Parameters
+    ----------
+    category : str
+        Category in data store, one of
+        ["0_raw", "1-preprocessed", "2_dataset"]
+    name : str
+        Name of dataset (subdir)
+    data_dir : bool
+        If True, the subdir "data" where data lives is added to the path
+
+    Returns
+    -------
+    PosixPath
+        Path to dataset
+    """
+    p = Path(get_abs_store_root_path()) / category / name
+    if data_dir is True:
+        p = p / "data"
+    return p
--- digipipe/workflow/utils.py
+++ digipipe/workflow/utils.py
@@ -3,13 +3,19 @@ Helper functions for the workflow
 """
 
 import os
-from digipipe.store.utils import get_abs_store_path
+from digipipe.store.utils import get_abs_store_root_path
 
 
 def search_data_workflows():
-    """Search for snakefiles (*.smk) in data store"""
+    """Search for snakefiles (*.smk) in data store.
+
+    Returns
+    -------
+    list
+        Paths to Snakemake files
+    """
     smk_files = []
-    for root, dirs, files in os.walk(get_abs_store_path()):
+    for root, dirs, files in os.walk(get_abs_store_root_path()):
         for file in files:
             if file.endswith(".smk") and ".TEMPLATE" not in str(root):
                 smk_files.append(os.path.join(root, file))

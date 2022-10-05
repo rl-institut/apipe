edbc0643297c0c581d4f6a14ded6e9eeb851a296
nesnoj <jonathan.amme@rl-institut.de>
2022-10-05 12:23:26 +0200

    Rename workflow helpers to utils

--- digipipe/workflow/utils.py
+++ digipipe/workflow/utils.py
@@ -2,15 +2,14 @@
 Helper functions for the workflow
 """
 
-import inspect
 import os
-from digipipe import store
+from digipipe.store.utils import get_abs_store_path
 
 
 def search_data_workflows():
     """Search for snakefiles (*.smk) in data store"""
     smk_files = []
-    for root, dirs, files in os.walk(os.path.dirname(inspect.getfile(store))):
+    for root, dirs, files in os.walk(get_abs_store_path()):
         for file in files:
             if file.endswith(".smk"):
                 smk_files.append(os.path.join(root,file))

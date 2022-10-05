c89dd5e760258a85ac93732e274f4313bd34b3e1
nesnoj <jonathan.amme@rl-institut.de>
2022-10-05 11:40:18 +0200

    Always use store's root dir for searching for snakefiles
    Requires store to be a module

--- digipipe/workflow/helpers.py
+++ digipipe/workflow/helpers.py
@@ -2,13 +2,15 @@
 Helper functions for the workflow
 """
 
+import inspect
 import os
+from digipipe import store
 
 
 def search_data_workflows():
     """Search for snakefiles (*.smk) in data store"""
     smk_files = []
-    for root, dirs, files in os.walk("../store/"):
+    for root, dirs, files in os.walk(os.path.dirname(inspect.getfile(store))):
         for file in files:
             if file.endswith(".smk"):
                 smk_files.append(os.path.join(root,file))

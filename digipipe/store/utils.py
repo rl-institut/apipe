6c06d53721ab4cd537d1a02997f3ef235383b2af
nesnoj <jonathan.amme@rl-institut.de>
2022-10-05 14:45:50 +0200

    Add functions to create tag strings for osmium and ogr2ogr for OSM data extraction

--- digipipe/store/utils.py
+++ digipipe/store/utils.py
@@ -41,3 +41,43 @@ def get_abs_dataset_path(category, name, data_dir=True):
     if data_dir is True:
         p = p / "data"
     return p
+
+
+def osm_create_tag_string_osmium(taglist):
+    """Create tag string required by osmium for extraction of OSM data.
+
+    Parameters
+    ----------
+    taglist : list of lists
+        List of tags, format: [[key_1, value_1], ... [key_n, value_n]]
+
+    Returns
+    -------
+    str
+        String with tags
+    """
+    tag_string = ""
+    for k, v in taglist:
+        tag_string += "=".join([k, str(v)]) + " "
+    tag_string = tag_string[:-1]
+    return tag_string
+
+
+def osm_create_tag_string_ogr(taglist):
+    """Create tag string required by ogr2ogr for extraction of OSM data.
+
+    Parameters
+    ----------
+    taglist : list of lists
+        List of tags, format: [[key_1, value_1], ... [key_n, value_n]]
+
+    Returns
+    -------
+    dict
+        Tags (key: "tags") and filter conditions (key: "conditions")
+    """
+    tag_conditions = "-where \"" + " OR ".join(
+        ["=".join(["\\\"" + tag + "\\\"", f"\\\"" + str(val) + "\\\""])
+         for tag, val in taglist]) + "\""
+    tags = ",".join([tag for tag, _ in taglist])
+    return {"tags": tags, "conditions": tag_conditions}

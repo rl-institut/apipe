0a6888820f3fbb634e07f011b14cdddf01be2230
MaGering <marie-claire.gering@rl-institut.de>
2022-11-01 11:57:48 +0100

    Add esys scripts

---
+++ digipipe/esys/scripts/postprocess.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+r"""
+Inputs
+-------
+optimized : str
+    ``results/{scenario}/optimized``: Directory containing dump of oemof.solph.Energysystem
+    with optimization results and parameters.
+scenario_name : str
+    ``{scenario}``: Name of the scenario.
+destination : str
+    ``results/{scenario}/postprocessed``: Target path for postprocessed results.
+logfile : str
+    ``logs/{scenario}.log``: path to logfile
+Outputs
+---------
+oemoflex.ResultsDatapackage
+    ResultsDatapackage
+Description
+-------------
+The script performs the postprocessing of optimization results.
+Explanations about the structure of the postprocessed data can be found in section
+:ref:`Postprocessed data` of the `docu <https://oemof-b3.readthedocs.io/en/latest/index.html>`_.
+"""
+import os
+import sys
+import pandas as pd
+
+from oemof.solph import EnergySystem
+from oemoflex.model.datapackage import ResultsDataPackage
+
+from digipipe.esys.esys.config import esys_conf
+
+if __name__ == "__main__":
+
+    optimized = sys.argv[1]
+
+    scenario_name = sys.argv[2]
+
+    destination = sys.argv[3]
+
+    logfile = sys.argv[4]
+    logger = esys_conf.add_snake_logger(logfile, "postprocess")
+
+    try:
+        es = EnergySystem()
+
+        es.restore(optimized)
+
+        rdp = ResultsDataPackage.from_energysytem(es)
+
+        rdp.set_scenario_name(scenario_name)
+
+        rdp.to_csv_dir(destination)
+
+        pd.Series({"objective": es.meta_results["objective"]}).to_csv(
+            os.path.join(destination, "objective.csv")
+        )
+
+    except:  # noqa: E722
+        logger.exception(
+            f"Could not postprocess data from energysystem in '{optimized}'."
+        )
+        raise

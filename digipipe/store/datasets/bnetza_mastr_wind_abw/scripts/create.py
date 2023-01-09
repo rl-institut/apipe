76e862f3e2b052434bb78b61bb517ed6b0b9e609
nesnoj <jonathan.amme@rl-institut.de>
2023-01-09 16:50:40 +0100

    Use common mastr functions in wind dataset

--- digipipe/store/datasets/bnetza_mastr_wind_abw/scripts/create.py
+++ digipipe/store/datasets/bnetza_mastr_wind_abw/scripts/create.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import geopandas as gpd
+from digipipe.scripts.datasets import mastr
 from digipipe.scripts.geo import (
     write_geofile,
     overlay
@@ -15,55 +16,20 @@ def process() -> None:
         dtype={"Postleitzahl": str},
     ).rename(columns=columns_dict).set_index("mastr_id")
 
+    units = mastr.add_voltage_level(
+        units=units,
+        locations_path=snakemake.input.locations,
+        gridconn_path=snakemake.input.gridconn
+    )
+
     # Drop units outside of Germany
     units = units.loc[(units.country == "Deutschland")]
 
-    # Get locations and grid connection point and merge both
-    locations = pd.read_csv(
-        snakemake.input.locations,
-        usecols=["MastrNummer", "Netzanschlusspunkte"],
-    ).rename(columns={"MastrNummer": "mastr_location_id2"})
-    gridconn = pd.read_csv(
-        snakemake.input.gridconn,
-        usecols=["NetzanschlusspunktMastrNummer", "Spannungsebene"]
-    )
-    locations = locations.merge(
-        gridconn,
-        left_on="Netzanschlusspunkte",
-        right_on="NetzanschlusspunktMastrNummer",
-        how="left",
-    ).drop_duplicates().rename(columns={"Spannungsebene": "voltage_level"})
-
-    # Add voltage level to units
-    units = units.merge(
-        locations[["mastr_location_id2", "voltage_level"]],
-        left_on="mastr_location_id",
-        right_on="mastr_location_id2",
-        how="left",
-    )
-
     # Add geometry and drop units without coords
-    units = units.loc[(~units.lon.isna() & ~units.lat.isna())]
-    units = gpd.GeoDataFrame(
-        units,
-        geometry=gpd.points_from_xy(
-            units["lon"], units["lat"], crs=4326
-        ),
-        crs=4326,
-    ).to_crs(3035)
-    units = units.rename(columns={"geometry": "geom"}).set_geometry("geom")
+    units = mastr.add_geometry(units)
 
-    # Drop unnecessary columns
-    units.drop(
-        columns=[
-            "mastr_location_id",
-            "mastr_location_id2",
-            "lon",
-            "lat",
-            "country",
-        ],
-        inplace=True,
-    )
+    # Do some basic filtering
+    units = mastr.cleanse(units)
 
     # Clip to ABW and add mun and district ids
     units = overlay(

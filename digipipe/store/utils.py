aa561d279335fab5286d4401fd1bdc08c1fb6e67
nesnoj <jonathan.amme@rl-institut.de>
2023-01-18 17:36:09 +0100

    Add function to merge DF string columns for unit aggregation

--- digipipe/store/utils.py
+++ digipipe/store/utils.py
@@ -5,6 +5,9 @@ Helper functions for datasets
 import inspect
 import os
 from pathlib import Path
+
+import pandas as pd
+
 from digipipe import store
 
 
@@ -95,3 +98,41 @@ PATH_TO_ABW_DISTRICTS_GPKG = get_abs_dataset_path(
     "bkg_vg250_districts_abw",
     data_dir=True
 ) / "bkg_vg250_districts_abw.gpkg"
+
+
+def df_merge_string_columns(
+        df: pd.DataFrame,
+        source_delimiter: str = ";",
+        target_delimiter: str = "; "
+) -> pd.Series:
+    """
+    Merge delimiter-separated strings in columns of DataFrame into new column
+    with unique values. Empty values are dropped.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe with columns to be merged
+    source_delimiter : str
+        Delimiter in original strings of all columns
+    target_delimiter : str
+        Desired delimiter in resulting Series
+
+    Returns
+    -------
+    pd.Series
+        Column with joined strings
+    """
+    for col in df.columns:
+        df[col] = df[col].apply(
+            lambda f: "|".join(
+                [_ for _ in set(f.split(source_delimiter)) if _ != ""]
+            ))
+    s = df.agg("|".join, axis=1)
+
+    return (
+        s.apply(
+            lambda f: target_delimiter.join(
+                [_ for _ in set(f.split("|")) if _ != ""]
+            ))
+    )

da57199edcc9d5ee04822ed8a4aebb90a5574d81
nesnoj <jonathan.amme@rl-institut.de>
2023-01-27 10:29:25 +0100

    Add district id to muns

--- digipipe/store/datasets/bkg_vg250_muns_abw/create.smk
+++ digipipe/store/datasets/bkg_vg250_muns_abw/create.smk
@@ -4,7 +4,7 @@ Snakefile for this dataset
 Note: To include the file in the main workflow, it must be added to the respective module.smk .
 """
 
-from digipipe.store.utils import get_abs_dataset_path
+from digipipe.store.utils import get_abs_dataset_path, PATH_TO_ABW_DISTRICTS_GPKG
 
 DATASET_PATH = get_abs_dataset_path("datasets", "bkg_vg250_muns_abw")
 
@@ -12,6 +12,8 @@ rule create:
     """
     Extract municipalities of ABW region
     """
-    input: rules.preprocessed_bkg_vg250_create.output
+    input:
+        abw_muns=rules.preprocessed_bkg_vg250_create.output,
+        abw_districts=PATH_TO_ABW_DISTRICTS_GPKG
     output: DATASET_PATH / "data" / "bkg_vg250_muns_abw.gpkg"
     script: DATASET_PATH / "scripts" / "create.py"
--- digipipe/store/datasets/bkg_vg250_muns_abw/scripts/create.py
+++ digipipe/store/datasets/bkg_vg250_muns_abw/scripts/create.py
@@ -1,34 +1,41 @@
-import sys
 import geopandas as gpd
 from digipipe.scripts.geo import (
     convert_to_multipolygon,
     write_geofile,
     rename_filter_attributes,
-    reproject_simplify
+    reproject_simplify,
+    overlay
 )
 
 
 def process():
-    data = gpd.read_file(infile, layer=config["layer"])
-    data = rename_filter_attributes(
-        gdf=data,
+    muns = gpd.read_file(snakemake.input.abw_muns[0], layer=config["layer"])
+    muns = rename_filter_attributes(
+        gdf=muns,
         attrs_filter_by_values=config["attributes_filter"],
         attrs_mapping=config["attributes"],
     )
-    data = reproject_simplify(
-        gdf=data,
+    muns = reproject_simplify(
+        gdf=muns,
         add_id_column=True,
     )
-    data = convert_to_multipolygon(data)
+
+    muns = overlay(
+        gdf=muns.rename(columns={"id": "mun_id"}),
+        gdf_overlay=gpd.read_file(snakemake.input.abw_districts),
+        retain_rename_overlay_columns={"id": "district_id"},
+        gdf_use_centroid=True,
+    ).rename(columns={"mun_id": "id"})
+
+    muns = convert_to_multipolygon(muns)
+
     write_geofile(
-        gdf=data,
-        file=outfile,
+        gdf=muns,
+        file=snakemake.output[0],
         layer_name=config["layer"],
     )
 
 
 if __name__ == "__main__":
-    infile = snakemake.input[0]
     config = snakemake.config
-    outfile = snakemake.output[0]
     process()

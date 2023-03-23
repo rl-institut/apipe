e0a3286f6159793f4002c8bad56c3cc2cd448a9e
MaGering <marie-claire.gering@rl-institut.de>
2023-03-23 16:00:11 +0100

    Add logging to config file

--- digipipe/config/__init__.py
+++ digipipe/config/__init__.py
@@ -1,7 +1,63 @@
+import logging
 import os
 from pathlib import Path
+import sys
 
 from digipipe.scripts.config import read_config
+
+
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        self.level = level
+        super(LevelFilter, self).__init__()
+
+    def filter(self, record):
+        return record.levelno != self.level
+
+
+LOGGING_LEVEL = 20  # Corresponds to the severity level "Info"
+
+root_logger = logging.getLogger()
+root_logger.setLevel(LOGGING_LEVEL)
+
+stream_formatter = logging.Formatter("%(levelname)s - %(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(stream_formatter)
+stream_handler.addFilter(LevelFilter(logging.ERROR))
+root_logger.addHandler(stream_handler)
+
+DEFAULT_LOGFILE = "snake.log"
+
+
+def add_snake_logger(logging_path, rulename):
+    """
+    Adds logging to file
+    Logfile is read from sys.argv, ending with ".log"
+    If this is None it is read from the logging path provided
+    This is done in order to add loggers for subprocesses,
+    where logfile is unknown.
+    """
+    logger = logging.getLogger(rulename)
+
+    # Try to obtain logging path from sys.argv and set it to None if can't
+    logfile = next(
+        (item for item in sys.argv if item.endswith(".log")), None
+    )
+    # If logging path not found in sys.argv
+    if logfile is None:
+        # If path provided calling function
+        if logging_path:
+            logfile = logging_path
+        else:
+            DEFAULT_LOGFILE
+
+    handler = logging.FileHandler(logfile)
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(file_formatter)
+    logger.addHandler(handler)
+    return logger
 
 
 def locate_global_config() -> Path:

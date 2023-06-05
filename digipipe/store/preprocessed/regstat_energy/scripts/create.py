a1ba5ea63eef845fde3712a640b219d818e40bb2
nesnoj <jonathan.amme@rl-institut.de>
2023-06-05 12:44:42 +0200

    Rename column

--- digipipe/store/preprocessed/regstat_energy/scripts/create.py
+++ digipipe/store/preprocessed/regstat_energy/scripts/create.py
@@ -16,20 +16,20 @@ def process() -> None:
     demand = demand.replace("-", 0).replace(".", np.nan)
 
     # Convert from GJ to MWh
-    demand = demand.set_index(["lau_code", "district"]).div(3.6).reset_index()
+    demand = demand.set_index(["lau_code", "name"]).div(3.6).reset_index()
 
     # Filter: federal states
     demand_states = demand.loc[demand.lau_code.str.len() == 2].set_index(
         "lau_code"
     )
-    demand_states.district = demand_states.district.str[2:]
+    demand_states.name = demand_states.name.str[2:]
     demand_states.to_csv(snakemake.output.demand_states)
 
     # Filter: districts
     demand_districts = demand.loc[demand.lau_code.str.len() == 5].set_index(
         "lau_code"
     )
-    demand_districts.district = demand_districts.district.str[6:]
+    demand_districts.name = demand_districts.name.str[6:]
     demand_districts.to_csv(snakemake.output.demand_districts)
 
 

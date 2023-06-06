8ce4990163af2c478e070487d8434ae353e97430
nesnoj <jonathan.amme@rl-institut.de>
2023-06-06 09:49:28 +0200

    Add industrial employment data to dataset employment_region

--- digipipe/store/datasets/employment_region/create.smk
+++ digipipe/store/datasets/employment_region/create.smk
@@ -16,7 +16,8 @@ rule create:
     Create employment dataset for region
     """
     input:
-        employment=get_abs_dataset_path("preprocessed", "ba_employment") / "data" / "employees.csv",
+        employment_total=get_abs_dataset_path("preprocessed", "ba_employment") / "data" / "employees.csv",
+        employment_ind=get_abs_dataset_path("preprocessed", "regiostat") / "data" / "employment_industry_muns.csv",
         region_muns=PATH_TO_REGION_MUNICIPALITIES_GPKG
     output:
         DATASET_PATH / "data" / "employees.csv"
--- digipipe/store/datasets/employment_region/scripts/create.py
+++ digipipe/store/datasets/employment_region/scripts/create.py
@@ -6,18 +6,47 @@ def process() -> None:
     # Get muns
     muns = gpd.read_file(snakemake.input.region_muns)
 
-    # Get employment data
-    employment = pd.read_csv(
-        snakemake.input.employment, index_col=0, dtype={"ags": str}
-    ).loc[muns.ags.to_list()][["employees_total"]]
+    # Get total employment data
+    employment_total = pd.read_csv(
+        snakemake.input.employment_total, index_col=0, dtype={"ags": str}
+    ).loc[muns.ags.to_list()]
+
+    # Get industrial employment data
+    employment_ind = pd.read_csv(
+        snakemake.input.employment_ind, index_col=0, dtype={"ags": str}
+    ).loc[muns.ags.to_list()][["employees_ind", "companies_ind"]]
+
+    # Fill missing data by average employees per company
+    employees_per_company = employment_ind.employees_ind.div(
+        employment_ind.companies_ind
+    )
+
+    if employees_per_company.isna().sum() > 0:
+        print(
+            f"WARNING: Number of employees or companies missing in "
+            f"{employees_per_company.isna().sum()} of {len(muns)} "
+            f"municipalities! Using average for employees per company "
+            f"({employees_per_company.mean().round(1)}) for those..."
+        )
+        employment_ind = employment_ind.assign(
+            employees_ind=employment_ind.employees_ind.fillna(
+                employment_ind.companies_ind * employees_per_company.mean()
+            )
+        )
+    employment_ind = employment_ind.assign(
+        employees_ind=employment_ind.employees_ind.round().astype(int)
+    )
+
+    # Join total data with industry data
+    employment = employment_total.join(employment_ind)
 
     # Join employment data with municipality ids
     employment = (
         muns.rename(columns={"id": "municipality_id"})
         .set_index("ags")
         .merge(employment, left_index=True, right_index=True)
-        .set_index("municipality_id")[["employees_total"]]
-    )
+        .set_index("municipality_id")
+    )[["employees_total", "companies_total", "employees_ind", "companies_ind"]]
 
     employment.to_csv(snakemake.output[0])
 

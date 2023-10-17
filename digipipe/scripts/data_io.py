dfca1d3dee9900c0635d095a3f8546678b22d1ca
David Starzl <david.starzl@rl-institut.de>
2023-10-17 13:24:01 +0200

    Fix issue ('file already exists' but does not)

--- digipipe/scripts/data_io.py
+++ digipipe/scripts/data_io.py
@@ -59,9 +59,7 @@ def copy_files(src_path: str, dest_path: str) -> None:
     """
     # Get a list of directories within source directory
     dir_list = [
-        d
-        for d in os.listdir(src_path)
-        if os.path.isdir(os.path.join(src_path, d))
+        d for d in os.listdir(src_path) if os.path.isdir(os.path.join(src_path, d))
     ]
     # Loop through each directory and copy files within `data` directory
     for d in dir_list:
@@ -73,22 +71,20 @@ def copy_files(src_path: str, dest_path: str) -> None:
             for file in os.listdir(src_dir):
                 src_file = os.path.join(src_dir, file)
                 dst_file = os.path.join(dst_dir, file)
-                if not os.path.isfile(dst_file):
-                    files_to_copy.append(file)
-                if file == ".gitkeep":
-                    continue
-                else:
+                if os.path.isfile(dst_file):
+                    if file == ".gitkeep":
+                        continue
                     print(f"\n'{file}' already exists in '{d}/data'.")
                     overwrite_file = input("Do you want to update it? (y/n) ")
                     while overwrite_file.lower() not in ["y", "n"]:
-                        overwrite_file = input(
-                            "Invalid input. Enter 'y' or 'n': "
-                        )
+                        overwrite_file = input("Invalid input. Enter 'y' or 'n': ")
                     if overwrite_file.lower() == "y":
                         shutil.copy(src_file, dst_file)
                         print(f"'{file}' updated.")
                     else:
                         continue
+                else:
+                    files_to_copy.append(file)
 
             for file in files_to_copy:
                 src_file = os.path.join(src_dir, file)
